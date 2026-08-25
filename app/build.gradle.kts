4ff8d1e2e334b576318ffe50a28a2e84b5e53512
KiritoXDone <58523182+KiritoXDone@users.noreply.github.com>
2026-08-25 13:48:13 +0800

    release: prepare Miaopu 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "dev.kiritoxd.miaopu"
         minSdk = 24
         targetSdk = 37
-        versionCode = 29
-        versionName = "1.0.4"
+        versionCode = 30
+        versionName = "1.0.5"
         manifestPlaceholders["appLabel"] = "喵扑"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
